6f7e3960e2200d5cbf8a41decc57bd77fd5c7c4d
arlentodd <76869256+arlentodd@users.noreply.github.com>
2024-11-11 16:32:44 +0800

    Update jd2.py

--- jd2.py
+++ jd2.py
@@ -3,7 +3,7 @@ import requests
 
 
 
-cookie = os.environ.get("JD_COOKIE2")
+cookie = os.environ.get("XIAO_K")
 
 url = ("https://api.m.jd.com/client.action?functionId=signBeanAct&body=%7B%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A%22-1"
        "%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda%22%3A%22-1"
